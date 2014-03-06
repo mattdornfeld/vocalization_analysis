53eb259be727a5ef54b2142e12bfb2b39bb3a50a
mdornfe1 <mdornfe1@gmail.com>
2014-03-06 18:12:35 -0500

    Split up cluster_parameter. Changed optimization algo from brute force to quasi newton. Got bootstrap working (I think).

--- bootstrap.py
+++ bootstrap.py
@@ -1,59 +1,34 @@
 import numpy as np
 from math import floor, ceil
 import matplotlib.pyplot as plt
-from matplotlib.path import Path
-import matplotlib.patches as patches
 import jump_interface
-import cluster_parameter
+import minimize_cost
 from IPython import embed
+
+DB_PATH = "/media/matthew/1f84915e-1250-4890-9b74-4bfd746e2e5a/jump.db"
+NUM_ITERATIONS = 200
 
 RAT_CLUSTERS = {'V1':[3,6], 'V2':[3,6], 'V3':[3,6], 'V4':[1,2,3,4,5,6,7,8],
         'V5':[3,6], 'V6':[1,2,3,4,5,6,7,8], 'V15':[1,3,6,8], 'V17':[3,6], 
         'V18':[3,6], 'V31':[1,3,6,8], 'V32': [3,6]}
 
-def resample(data, block_size, num_blocks):
-    x1_min = floor(np.amin(data[:,0]))
-    x1_max = ceil(np.amax(data[:,0]))
-    x2_min = floor(np.amin(data[:,1]))
-    x2_max = ceil(np.amax(data[:,1]))
+def resample(data):
+    idx = np.random.randint(0, len(data), len(data))
 
-    x1_blocks = np.arange(x1_min, x1_max, block_size)
-    x2_blocks = np.arange(x2_min, x2_max, block_size)
-    
-    num_rows = len(x1_blocks)
-    num_cols = len(x2_blocks)
+    return data[idx,:]
 
-    x1_idx = np.random.randint(0, num_rows-1, num_blocks)
-    x2_idx = np.random.randint(0, num_cols-1, num_blocks)
-    
-    verts = []
-    codes = []
+def find_std(measurements, NUM_ITERATIONS):
+    u = np.mean(measurements)
 
-    for i, j in zip(x1_idx, x2_idx):
-    	verts += [(x1_blocks[i], x2_blocks[j])]
-    	verts += [(x1_blocks[i], x2_blocks[j+1])]
-    	verts += [(x1_blocks[i+1], x2_blocks[j+1])]
-    	verts += [(x1_blocks[i+1], x2_blocks[j])]
-    	verts += [(x1_blocks[i], x2_blocks[j])]
-    	codes += [Path.MOVETO, Path.LINETO, Path.LINETO, Path.LINETO, Path.LINETO]
+    return np.sqrt(sum((measurements-u)**2)/(NUM_ITERATIONS-1))
 
-    poly = Path(verts, codes)
-    
-    return data[poly.contains_points(data)]
-
-def find_std(params, num_iterations):
-    u = np.mean(params)
-
-    return np.sqrt(sum((params-u)**2)/(num_iterations-1))
-
-def confidence_interval(params, confidence):
+def confidence_interval(measurements, confidence):
     fig1, ax1 = plt.subplots()
-    bins, edges, _ = ax1.hist(params, bins=np.sqrt(len(params)), 
+    bins, edges, _ = ax1.hist(measurements, bins=np.sqrt(len(measurements)), 
         normed=True, cumulative=True)
     fig2, ax2 = plt.subplots()
-    ax2.hist(params, bins=np.sqrt(len(params)),
+    ax2.hist(measurements, bins=np.sqrt(len(measurements)),
         normed=True, cumulative=False)
-
 
     a = sum(bins < confidence)
     a = edges[a]
@@ -63,38 +38,42 @@ def confidence_interval(params, confidence):
 
     return [a,b]
 
-def main(rat, estimator):
+def main(data, estimator):
     if not hasattr(estimator, '__call__'):
-        raise Exception("estimator must be a function")
-    if not isinstance(rat, str):
-        raise Exception("rat must be a string")
-    dbPath = '/media/matthew/1f84915e-1250-4890-9b74-4bfd746e2e5a/jump.db'
-    ji = jump_interface.JumpInterface(dbPath)
-    jumps = ji.get_jumps(rat)[:,0:2]
-    block_size = 300
-    num_blocks = 10000
-    num_iterations = 100
+        raise Exception("estimator must be a function")    
 
     theta = []
-    intercept = []
-    for n in range(num_iterations):
+    b = []
+    error_count = 0
+    
+    for n in range(NUM_ITERATIONS):
         if n%10 == 0: print(n)
-        resampled_data = resample(jumps, block_size, num_blocks)
-        t, i = estimator(resampled_data, RAT_CLUSTERS[rat])
-        theta.append(t)
-        intercept.append(i)
+        try:
+            resampled_data = resample(jumps)
+            t, i = estimator(resampled_data, RAT_CLUSTERS[rat])
+            theta.append(t)
+            b.append(i)
+        except:
+            error_count += 1
 
-    [a_theta, b_theta] = confidence_interval(theta, 0.05)
-    [a_intercept, b_incercept] = confidence_interval(intercept, 0.05)
-    #print(a_theta)
-    #print(b_theta)
-    #print(a_incercept)
-    #print(b_intercept)
+    theta_confidence = confidence_interval(theta, 0.05)
+    b_confidence = confidence_interval(b, 0.05)
+    
+    print('error_count=' + str(error_count))
+    print('theta_mean=' + str(sum(theta)/len(theta)))
+    print('theta_confidence=' + str(theta_confidence))
+    print('b_mean=' + str(sum(b)/len(b)))
+    print('b_confidence=' + str(b_confidence))
+
+
     plt.show()
 
 if __name__ == '__main__':
     rat = 'V6'
-    main(rat, cluster_parameter.main)
+    ji = jump_interface.JumpInterface(DB_PATH)
+    jumps = ji.get_jumps(rat)[:,0:2]
+
+    main(jumps, minimize_cost.main)
 
 
 
@@ -114,4 +93,29 @@ ax.add_patch(patch)
 ax.set_xlim(f1_min, f1_max)
 ax.set_ylim(f2_min, f2_max)
 plt.show()
+    x1_min = floor(np.amin(data[:,0]))
+    x1_max = ceil(np.amax(data[:,0]))
+    x2_min = floor(np.amin(data[:,1]))
+    x2_max = ceil(np.amax(data[:,1]))
+
+    x1_blocks = np.arange(x1_min, x1_max, BLOCK_SIZE)
+    x2_blocks = np.arange(x2_min, x2_max, BLOCK_SIZE)
+
+    num_rows = len(x1_blocks)
+    num_cols = len(x2_blocks)
+      x1_idx = np.random.randint(0, num_rows-1, NUM_BLOCKS)
+    x2_idx = np.random.randint(0, num_cols-1, NUM_BLOCKS)
+    
+    verts = []
+    codes = []
+
+    for i, j in zip(x1_idx, x2_idx):
+        verts += [(x1_blocks[i], x2_blocks[j])]
+        verts += [(x1_blocks[i], x2_blocks[j+1])]
+        verts += [(x1_blocks[i+1], x2_blocks[j+1])]
+        verts += [(x1_blocks[i+1], x2_blocks[j])]
+        verts += [(x1_blocks[i], x2_blocks[j])]
+        codes += [Path.MOVETO, Path.LINETO, Path.LINETO, Path.LINETO, Path.LINETO]
+
+    poly = Path(verts, codes)
 """
---
+++ minimize_cost.py
@@ -0,0 +1,185 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.mlab import find
+import sqlite3 as lite
+import jump_interface
+from IPython import embed
+from functools import partial
+import brewer2mpl as brew
+from collections import OrderedDict
+from scipy.optimize import fmin_ncg
+import poly_cluster
+
+global theta_vec, error
+theta_vec = []
+error = []
+
+def slope(n1, n2, theta):
+	return float(n2-theta) / (n1-theta)
+
+def dslope(n1, n2, theta):
+	return float(n1-n2) / (n1 - theta)**2
+
+SLOPES = OrderedDict([(0, partial(slope,2,1)), (1, partial(slope,3,2)), 
+	(2, partial(slope,4,3)), (3, partial(slope,5,4)), (4,partial(slope,6,5)), 
+	(5, partial(slope,5,6)), (6,partial(slope,4,5)), (7,partial(slope,3,4)), 
+	(8,partial(slope,2,3)), (9, partial(slope,1,2))])
+
+DSLOPES = OrderedDict([(0, partial(dslope,2,1)), (1, partial(dslope,3,2)), 
+	(2, partial(dslope,4,3)), (3, partial(dslope,5,4)), (4,partial(dslope,6,5)), 
+	(5, partial(dslope,5,6)), (6,partial(dslope,4,5)), (7,partial(dslope,3,4)), 
+	(8,partial(dslope,2,3)), (9, partial(dslope,1,2))])
+
+COLOR_MAP = (brew.get_map('Set3', 'qualitative', 10).mpl_colors +
+	brew.get_map('Accent', 'qualitative', 6).mpl_colors)
+
+LEGEND = OrderedDict([(-1,("unclustered", COLOR_MAP[2])), (0,("2 to 1", COLOR_MAP[0])), 
+(1,("3 to 2", COLOR_MAP[15])), (2,("4 to 3", COLOR_MAP[3])), 
+(3,("5 to 4", COLOR_MAP[4])), (4,("6 to 5", COLOR_MAP[5])), 
+(5,("5 to 6", COLOR_MAP[6])), (6,("4 to 5", COLOR_MAP[7])), 
+(7,("3 to 4", COLOR_MAP[8])), (8,("2 to 3", (131./255,39./255,39./255))), 
+(9,("1 to 2", COLOR_MAP[10]))])
+
+REVERSE_LEGEND = OrderedDict([("unclustered", -1), ("2 to 1", 0), ("3 to 2", 1), ("4 to 3", 2), 
+("5 to 4", 3), ("6 to 5", 4), ("5 to 6", 5), ("4 to 5", 6), ("3 to 4", 7), ("2 to 3", 8),
+("1 to 2", 9)])
+
+NUM_CLUSTERS = len(LEGEND) - 1
+
+#Error of all the clusters. We're trying to minimize this.
+def dcost(x, *args):
+	theta = x[0]
+	b = x[1]
+	jumps = args[0]
+	included_clusters = args[1]
+
+	h = [(n,SLOPES[n](theta)) for n in included_clusters]
+	h = OrderedDict(h)
+	dh = [(n, DSLOPES[n](theta)) for n in included_clusters]
+	dh = OrderedDict(dh)
+	
+	cluster = poly_cluster.main(jumps, h, included_clusters)
+
+	dcost_theta = 0
+	dcost_b = 0
+	for c in h.keys(): 
+		idx = find(cluster==c)
+		l = len(jumps)
+		for j in jumps[idx,:]:
+			x = (j[0] / h[c] + j[1] - b) / (h[c] + 1 / h[c])
+			y = h[c] * x + b 
+			
+			dx_theta = ((h[c]**2 + 1) * (j[1] - b) * dh[c] - 
+				2 * (j[0] + (j[1] - b) * h[c]) * h[c] * dh[c]) / (h[c]**2 + 1)**2
+			
+			dy_theta = ((h[c]**2 + 1) * (j[0] * dh[c] + 2 * (j[1] - b) * h[c] * dh[c]) 
+				- 2 * (j[0] * h[c] + (j[1] - b) * h[c]**2) * h[c] * dh[c]) / (h[c]**2 + 1)**2
+			dx_b = -h[c] / (h[c]**2 + 1)
+			dy_b = -h[c]**2 / (h[c]**2 + 1)
+
+			dcost_theta += (2 * (j[0] - x) * dx_theta 
+				+ 2 * ((j[1] - b) - y) * dy_theta) / l
+
+			dcost_b += (2 * (j[0] - x) * dx_b 
+				+ 2 * ((j[1] - b) - y) * dy_b) / l
+
+	return np.array([dcost_theta, dcost_b])
+
+def cost(x, *args):
+	theta = x[0]
+	b = x[1]
+	jumps = args[0]
+	included_clusters = args[1]
+	h = [(n,SLOPES[n](theta)) for n in included_clusters]
+	h = OrderedDict(h)
+
+	cluster = poly_cluster.main(jumps, h, included_clusters)
+
+	cost = 0 
+	for c in h.keys(): 
+		idx = find(cluster==c)
+		l = len(jumps)
+		for j in jumps[idx,:]:
+			x = (j[0] / h[c] + j[1] - b) / (h[c] + 1 / h[c])
+			y = h[c] * x + b 
+			cost += (((j[0] - x)**2 + (j[1]-y)**2)) / l 
+
+	return cost
+
+def callback(xk):
+	global theta_vec, error
+	theta_vec += [xk[0]]
+	error += [cost(xk, jumps, included_clusters)]
+
+
+
+def main(jumps, included_clusters = range(NUM_CLUSTERS)):
+	theta_min, b_min = fmin_ncg(f=cost, x0=[0.5,0], fprime=dcost,
+	 args=(jumps, included_clusters), avextol=1e-5, disp=0, callback=None)
+	
+theta = np.arange(0,1,0.01)
+error = np.array([cost([t,10], jumps, included_clusters) for t in theta])
+derror = np.array([dcost([t,10], jumps, included_clusters)[0] for t in theta])
+
+plt.plot(theta, error)
+for i in range(0,len(derror),10):
+	plt.arrow(theta[i], error[i], 1/10., derror[i]/10, shape='full')
+	plt.show()
+
+	print(theta_min)
+	print(b_min)
+	return theta_min, b_min
+
+def plot_jumps(jumps, cluster, slopes):
+	num_clusters = len(slopes)
+	fig1 = plt.figure(figsize=(12, 12), dpi=100)
+
+	ax1 = fig1.add_subplot(111, aspect='equal')
+	#ax1.set_title('After Jump Frequency Vs Before Jump Frequency')
+	ax1.set_xlabel('f1', size=12)
+	ax1.set_ylabel('f2', size=12)
+	#transitions = [str(n+1)+' to ' +str(n) for n in range(2,6)]
+	#transitions = transitions+[str(n)+' to ' +str(n+1) for n in range(5,1,-1)]
+
+	f = np.arange(20e3, 80e3, 100)
+	for c in slopes.keys():
+		#plot the data set a second time and color code them by cluster
+		idx = find(cluster==c)
+		if len(idx)==0: continue
+		minf = np.amin(jumps[idx,:][:,0])
+		maxf = np.amax(jumps[idx,:][:,0])
+		line, = ax1.plot(jumps[idx,:][:,0],jumps[idx,:][:,1], 'o', 
+			markersize=2.3, color = LEGEND[c][1], label=LEGEND[c][0])
+		line, = ax1.plot(f, slopes[c]*f, color = LEGEND[c][1])
+
+	#create legend ax_jumps.legend()
+	handles, labels = ax1.get_legend_handles_labels()
+	rect = [0.73, 0.758, 0.1, 0.15] #l,b,w,h
+	ax_legend = fig1.add_axes(rect)
+	ax_legend.get_xaxis().set_visible(False)
+	ax_legend.get_yaxis().set_visible(False)
+	ax_legend.set_axis_bgcolor((0.75, 0.75, 0.75, 1.0))
+	ax_legend.set_frame_on(False)
+	ax_legend.legend(handles[::-1], labels[::-1],
+	 markerscale=3, numpoints=1)
+
+	#set axis limits
+
+	ax1.set_xlim(f[0], f[-1])
+	ax1.set_ylim(f[0], f[-1])
+	plt.show()
+	#fig1.savefig(rat+'.png', bbox_inches='tight', pad_inches=0)	
+
+if __name__ == "__main__":
+	dbPath = '/media/matthew/1f84915e-1250-4890-9b74-4bfd746e2e5a/jump.db'
+	ji = jump_interface.JumpInterface(dbPath)
+	"""
+	rat_clusters={'V1':[3,6], 'V2':[3,6], 'V3':[3,6], 'V4':[1,2,3,4,5,6,7,8],
+		'V5':[3,6], 'V6':[1,2,3,4,5,6,7,8], 'V15':[1,3,6,8], 'V17':[3,6], 
+		'V18':[3,6], 'V31':[1,3,6,8], 'V32': [3,6]}
+	"""
+	rat_clusters={'V6':[1,2,3,4,5,6,7,8]}
+	for rat, included_clusters in rat_clusters.iteritems():
+		jumps = ji.get_jumps(rat)[:,0:2]
+		main(jumps, included_clusters)
+
